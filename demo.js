f7dc20669a6e79f585d34834593c97619d088e65
hefeng <hefeng@agoravoice.io>
2015-01-04 23:06:05 +0800

    reduce memory overhead by storing only string

--- demo.js
+++ demo.js
@@ -2,9 +2,14 @@ var IpParser=require('./ipparser.js');
 
 
 var test = function(){
-    var ip = 16813053;
+    var ip = 16779263;
     var d = IpParser.getIpInfo(ip);
     console.log('ip info for:%s - %s',ip, JSON.stringify(d));
 }
 // IpParser.init('resources/ips.dat', test);
 IpParser.init('/tmp/ip.txt', test);
+
+var loop = function(){
+    setTimeout(loop, 1000);
+}
+loop();
